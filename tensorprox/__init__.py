4dbee4c7dd35db1654d0b555bae57adaef901be7
borgg-dev <data.deepmind@gmail.com>
2025-04-08 19:49:07 +0100

    lightweight lockdown+revert

--- tensorprox/__init__.py
+++ tensorprox/__init__.py
@@ -13,17 +13,17 @@ __spec_version__ = _version_to_int(__version__)
 
 #Inner parameters
 EPSILON: int = 15
-DELTA: int = 5
-CHALLENGE_DURATION: int = 10 #15 minutes
+DELTA: int = 15
+CHALLENGE_DURATION: int = 60 #15 minutes
 MAX_TGENS = 2
 
 #Timeouts
 ROUND_TIMEOUT: int = 300 #150 blocks / 30 minutes
 INITIAL_SETUP_TIMEOUT: int = 60 # 30 seconds
-LOCKDOWN_TIMEOUT: int = 180
+LOCKDOWN_TIMEOUT: int = 60
 GRE_SETUP_TIMEOUT: int = 180
 CHALLENGE_TIMEOUT: int = CHALLENGE_DURATION + DELTA
-REVERT_TIMEOUT: int = 120
+REVERT_TIMEOUT: int = 60
 
 # Store the base path dynamically, assuming `tensorprox` is the base directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
