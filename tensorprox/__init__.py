e3bcf98f52900170d1c0759f3f917aff9db64fa9
borgg-dev <data.deepmind@gmail.com>
2025-04-25 21:12:30 +0000

    add data commit for synchronized active validators count

--- tensorprox/__init__.py
+++ tensorprox/__init__.py
@@ -12,14 +12,14 @@ __version__ = "0.1.0"
 __spec_version__ = _version_to_int(__version__)
 
 #Inner parameters
-EPSILON: int = 180 #10 minutes
+EPSILON: int = 180 #3 minutes
 DELTA: int = 240 #3 minutes
 CHALLENGE_DURATION: int = 900 #15 minutes
 MIN_TGENS = 2
 MAX_TGENS = 8
 
 #Timeouts
-ROUND_TIMEOUT: int = 1980 # 26 minutes
+ROUND_TIMEOUT: int = 2200 # 37 minutes
 INITIAL_SETUP_TIMEOUT: int = 120 # 2 minutes
 LOCKDOWN_TIMEOUT: int = 120 #1 minutes
 GRE_SETUP_TIMEOUT: int = 240 #4 minutes
